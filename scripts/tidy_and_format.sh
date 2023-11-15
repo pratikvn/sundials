8a439756c73436d8bc510b16f742da1a771c0667
Balos, Cody, J <balos1@llnl.gov>
2023-11-15 02:44:32 -0700

    add directory option to tidy and format script

--- scripts/tidy_and_format.sh
+++ scripts/tidy_and_format.sh
@@ -2,6 +2,6 @@
 
 # We require clang-format v17. Other versions may produce different styles!
 
-find .. -name fmod -prune -iname *.h -o -iname *.hpp -o -iname *.c -o -iname *.cpp -o -iname *.cuh -o -iname *.cu | xargs clang-tidy -fix-errors -p builddir/
+find $1 -name fmod -prune -iname *.h -o -iname *.hpp -o -iname *.c -o -iname *.cpp -o -iname *.cuh -o -iname *.cu | xargs clang-tidy -fix-errors -p builddir/
 
-find .. -name fmod -prune -iname *.h -o -iname *.hpp -o -iname *.c -o -iname *.cpp -o -iname *.cuh -o -iname *.cu | xargs clang-format -i
+find $1 -name fmod -prune -iname *.h -o -iname *.hpp -o -iname *.c -o -iname *.cpp -o -iname *.cuh -o -iname *.cu | xargs clang-format -i
