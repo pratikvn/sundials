5177e82ce17b270ae87c5028cf9e67ec16fde15f
Balos, Cody, J <balos1@llnl.gov>
2023-11-15 11:20:18 -0700

    specify clang format version we need

--- scripts/tidy_and_format.sh
+++ scripts/tidy_and_format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# We require clang-format v17. Other versions may produce different styles!
+# We require clang-format 17.0.4. Other versions may produce different styles!
 
 find $1 -name fmod -prune -iname *.h -o -iname *.hpp -o -iname *.c -o -iname *.cpp -o -iname *.cuh -o -iname *.cu | xargs clang-tidy -fix-errors -p builddir/
 
